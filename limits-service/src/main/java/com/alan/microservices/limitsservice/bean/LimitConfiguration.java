a5319b252473d935f1f569311c18b20ddabdd821
Alan Perius <alan.perius.mv@gmail.com>
2019-11-05 10:46:20 -0300

    My First Commit

---
+++ limits-service/src/main/java/com/alan/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,16 @@
+package com.alan.microservices.limitsservice.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LimitConfiguration {
+
+    private int maximum;
+    private int minimum;
+
+}
